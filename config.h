f4376bc30c291f2ca1d961b826906469cbcbad33
ricson <80329621+satancci@users.noreply.github.com>
2024-11-26 15:43:46 -0300

    create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define variavel1 "valor_variavel1"
+#define variavel2 "valor_variavel2"
+#define variavel3 "valor_variavel3"
+#define variavel4 "valor_variavel4"
+
+
+#endif
